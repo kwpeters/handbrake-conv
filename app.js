91cbe6843f623ddce1f0bbca1a0fcb5afda1ad87
Kevin Peters <kwpeters@Kevins-MacBook-Pro.local>
2015-10-03 17:19:22 -0400

    Added config module.

--- app.js
+++ app.js
@@ -1,50 +1,135 @@
-var hbjs = require('handbrake-js');
+var path        = require('path'),
+    hbjs        = require('handbrake-js'),
+    _           = require('lodash'),
+    conversions = require('./config'),
+    defaultConversionOptions = {
+        preset: "Universal",
+        encoder: 'x264'
+    };
+
+doIt();
+
+function doIt() {
+    //
+    // If there are any conversion consisting of just a string, assume that the
+    // string is the path to the input file.
+    //
+    conversions = _.map(conversions, function (curConversion) {
+        var conversionObj = {};
+
+        // If the current conversion is an object, do nothing.
+        if (_.isObject(curConversion)) {return curConversion;}
+
+        if (_.isString(curConversion)) {
+            conversionObj = {
+                input: curConversion
+            };
+        } else {
+            throw new Error('Unknown configuration specified: ' + curConversion);
+        }
+
+        return conversionObj;
+    });
+
+    //
+    // Assign default conversion options for any that have not been specified.
+    //
+    conversions = _.map(conversions, function (curConversion) {
+        _.defaults(curConversion, defaultConversionOptions);
+        return curConversion;
+    });
 
 
-var info = {
-    input: 'Bubble Guppies s02e02.m4v',
-    output: 'Bubble Guppies s02e02-test.mkv'
-};
+    //
+    // If output was not specified, assume a .mkv file in the same directory as the
+    // input.
+    //
+    conversions = _.map(conversions, function (curConversion) {
+        var inputPathParts;
 
-var hbConfig = {
-    input: info.input,
-    output: info.output,
-    preset: "Universal",
-    encoder: 'x264'
-};
+        // If the current conversion has an output file specified, do nothing.
+        if (curConversion.output) {return curConversion;}
 
-var hb = hbjs.spawn(hbConfig);
-hb.on('error', function onError(err) {
-    console.log('Error!');
-    console.log(err);
-});
+        inputPathParts = splitPath(curConversion.input);
+        curConversion.output = inputPathParts.dirName + '/' + inputPathParts.baseName + '.mkv';
 
-hb.on('start', function onStart() {
-    console.log('Start (CLI launched)');
-});
+        return curConversion;
+    });
 
-hb.on('begin', function onBegin() {
-    console.log('Begin (encoding has begun)');
-});
 
-hb.on('progress', function onProgress(progress) {
-    console.log(
-        "Task %d of %d, percent complete: %s, ETA: %s",
-        progress.taskNumber,
-        progress.taskCount,
-        progress.percentComplete,
-        progress.eta
-    );
-});
-hb.on('output', function onOutput(output) {
-    console.log('Output:');
-    console.log(output);
-});
+    conversions.forEach(function (curConversion) {
+        var handbrake;
+        console.log('--------------------------------------------------------------------------------');
+        console.log('Starting conversion:');
+        console.dir(curConversion);
 
-hb.on('end', function onEnd() {
-    console.log('End (encoding completed)');
-});
+        //runHandbrake(curConversion);
+    });
+}
 
-hb.on('complete', function onComplete() {
-    console.log('Complete (CLI exited)');
-});
+////////////////////////////////////////////////////////////////////////////////
+//
+// Returns an object with properties: dirName, baseName and extName
+//
+////////////////////////////////////////////////////////////////////////////////
+function splitPath(filePath) {
+    var dirName,
+        baseName,
+        extName;
+
+    dirName = path.dirname(filePath);
+    extName = path.extname(filePath);
+    baseName = path.basename(filePath, extName);
+
+    return {
+        dirName: dirName,
+        baseName: baseName,
+        extName: extName
+    };
+}
+
+////////////////////////////////////////////////////////////////////////////////
+//
+// Runs Handbrake with the specified configuration.
+//
+////////////////////////////////////////////////////////////////////////////////
+function runHandbrake(config) {
+
+    handbrake = hbjs.spawn(config);
+
+    handbrake.on('error', function onError(err) {
+        console.log('Error!');
+        console.log(err);
+    });
+
+    handbrake.on('start', function onStart() {
+        //console.log('Start (CLI launched)');
+    });
+
+    handbrake.on('begin', function onBegin() {
+        console.log('Begin (encoding has begun)');
+    });
+
+    handbrake.on('progress', function onProgress(progress) {
+        // console.log(
+        //     "Task %d of %d, percent complete: %s, ETA: %s",
+        //     progress.taskNumber,
+        //     progress.taskCount,
+        //     progress.percentComplete,
+        //     progress.eta
+        // );
+    });
+
+    handbrake.on('output', function onOutput(output) {
+        console.log(output);
+    });
+
+    handbrake.on('end', function onEnd() {
+        //console.log('End (encoding completed)');
+    });
+
+    handbrake.on('complete', function onComplete() {
+        //console.log('Complete (CLI exited)');
+    });
+
+}
---
+++ config.js
@@ -0,0 +1,9 @@
+var conversions = [];
+
+conversions.push('movie1.m4v');
+conversions.push({
+    input: 'movie2.m4v',
+    output: 'movie2.mkv'
+});
+
+module.exports = conversions;
